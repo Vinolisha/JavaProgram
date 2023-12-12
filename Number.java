fcf5e7e7f8e984e921f32c85fcdb9331cf301b8f
Vinolisha <153608805+Vinolisha@users.noreply.github.com>
2023-12-12 18:31:34 +0530

    Create Number.java

---
+++ Number.java
@@ -0,0 +1,10 @@
+public class Number{
+  public void main(String arg[]){
+    int num=20;
+    System.out.println("List of Even number:" + num);
+    if(int i=0;i<num;i++){
+      if(i%2==0)
+        System.out.println(i+" ");
+    }
+  }
+      
